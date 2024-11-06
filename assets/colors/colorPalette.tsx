874c69d01643e6ebea74383c8feb2a5d266c42ba
JSebastianPaezSalesianoDev <usr3766@salesianos-lacuesta.net>
2024-11-06 10:14:58 +0000

    feat(colorPalette): Se han agregado los colores que potencialmente se usaran

--- assets/colors/colorPalette.tsx
+++ assets/colors/colorPalette.tsx
@@ -1,4 +1,10 @@
 enum colors {
   black = "",
   white = "",
+  greenCheck = "",
+  pinky = "#FFE3E3",
+  lightBlue = "#C4E1F6",
+  palePink = "#FFF5CD",
+  tannedPink = "#FFCFB3",
+  strangeBlue = "#9FD8E6",
 }
